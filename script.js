02ab5cdb880d9943d09bf979b4bd7d85f7b4a35f
Mauro Diogo <mauromjdiogo@gmail.com>
2025-03-22 18:10:49 -0300

    expenseAdd function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
+const expenseList = document.querySelector("ul")
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, "")
@@ -23,7 +24,7 @@ function formatCurrencyBRL(value) {
 form.onsubmit = event => {
     event.preventDefault()
 
-    const newExpenses = {
+    const newExpense = {
         id: new Date().getTime(),
         expense: expense.value,
         category_id: category.value,
@@ -32,5 +33,45 @@ form.onsubmit = event => {
         created_at: new Date(),
     }
 
-    console.log(newExpenses)
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        const expenseItem = document.createElement('li')
+        expenseItem.classList.add('expense')
+
+        const expenseIcon = document.createElement('img')
+        expenseIcon.setAttribute('src', `./img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute('alt', newExpense.category_name)
+
+        const expenseInfo = document.createElement('div')
+        expenseInfo.classList.add('expense-info')
+
+        const expenseName = document.createElement('strong')
+        expenseName.textContent = newExpense.expense
+
+        const expenseCategory = document.createElement('span')
+        expenseCategory.textContent = newExpense.category_name
+
+        expenseInfo.append(expenseName, expenseCategory)
+
+        const expenseAmount = document.createElement('span')
+        expenseAmount.classList.add('expense-amount')
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+            .toUpperCase()
+            .replace('R$', '')}`
+
+        const removeIcon = document.createElement('img')
+        removeIcon.classList.add('remove-icon')
+        removeIcon.setAttribute('src', './img/remove.svg')
+        removeIcon.setAttribute('alt', 'Remover despesa')
+
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
+
+        expenseList.append(expenseItem)
+
+    } catch (error) {
+        console.log(error)
+    }
 }
