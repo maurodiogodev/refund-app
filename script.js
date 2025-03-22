05da6628283230e21596a49f4b384a3540903618
Mauro Diogo <mauromjdiogo@gmail.com>
2025-03-22 16:55:46 -0300

    getting expenses datas from the form

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
-const category =
+const category = document.getElementById("category")
 
-    amount.oninput = () => {
-        let value = amount.value.replace(/\D/g, "")
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "")
 
-        value = Number(value) / 100
+    value = Number(value) / 100
 
-        amount.value = formatCurrencyBRL(value)
-    }
+    amount.value = formatCurrencyBRL(value)
+}
 
 function formatCurrencyBRL(value) {
     value = value.toLocaleString('pt-BR', {
@@ -19,3 +20,17 @@ function formatCurrencyBRL(value) {
     return value
 }
 
+form.onsubmit = event => {
+    event.preventDefault()
+
+    const newExpenses = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    console.log(newExpenses)
+}
